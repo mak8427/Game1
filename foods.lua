3cd4c291630c503278f4110fa8c84f222094a446
mak8421 <davide.mattioli.2001@gmail.com>
2022-03-22 22:18:07 +0100

    added farm industry

---
+++ foods.lua
@@ -0,0 +1,12 @@
+local function good(stinga)
+    if stringa=='ananas' then
+        a=1.5
+    end
+    a=1.5
+    return a
+end
+
+return {good=good()}
+
+
+
--- main.lua
+++ main.lua
@@ -1,26 +1,46 @@
+local foods = require "foods"
+
+
+
 function love.load()
+    local foods = require "foods"
     pause = false
-    pops=10
-    food=500
     font=love.graphics.newFont(40)
     throughtput=1.0
     food_consumed_pop=0.5
-    multi=1
+    multi=3
     cycles=0
     coefficent_innovation=0.01
     coefficent_risparmio=0
-    Pop={Food=500, Number=10}
+    food_price=3
+    Pop={Food=500, Number=10, t_1=0}
     Pop1=Pop
+    Pops={}
+    for i= 1,3 do
+        Pops[i]=Pop
+    end
+
+    farm_industry= {pops=pops, out=0 ,size=1, food_type=0.9}
+
+
+    function farm_industry.Update()
+        function farm_industry.Output()
+            farm_industry.out=farm_industry.pops*food_type
+        end
+        farm_industry.Output()
+
+    end
+
 
     function throughtput(pop,cylces)
-        x=(math.pow((1/1.01),pop)+(math.cos(pop)+1)/50+ math.random(-2,5))*(multi);  
+        x=(math.pow((1/1.01),pop)+(math.cos(pop)+1)/50+ math.random(0.1,0.2))*(multi);  
         return x
     end
 
     function Pop.Update()
         function Pop.food()
             Pop.t_1=throughtput(Pop.Number,cycles)
-            food_produced=Pop.Number*Pop.t_1
+            food_produced=(Pop.Number+1)*Pop.t_1
             food_consumed=(food_consumed_pop+math.log(Pop.Number+1))*Pop.Number
             delta=food_produced-food_consumed
             Pop.Food=Pop.Food+delta
@@ -29,7 +49,7 @@ function love.load()
             end
         end
         function Pop.Change()
-            pops_variation=math.floor(((Pop.Food+delta)/food_consumed_pop)/50)
+            pops_variation=math.floor(((Pop.Food+delta)/food_consumed_pop)/5)
             Pop.Number=Pop.Number+pops_variation
             if Pop.Number<=0 then
                 Pop.Number=1
@@ -48,15 +68,7 @@ end
 
 
 
-local function pop_variations(food_consumed_pop,food)
-    pop_variation=math.floor((food/food_consumed_pop)/50)
-    return pop_variation
 
-end
-
-function innovation(money,coefficent_innovation)
-    a=1
-end
 
 
 
@@ -64,24 +76,13 @@ end
 
 function love.update(dt)
     --love.timer.sleep(0.05)
-    Pop1.Update()
     function love.keypressed(key, unicode)
 	 	if key == 'p' then 
             pause = not pause 
         end
     end
-    if pause==false then
-        t_1=throughtput(pops,cycles)
-        food_produced=pops*t_1
-        food_consumed=(food_consumed_pop+math.log(pops))*pops 
-        delta=food_produced-food_consumed
-        food=food+delta
-        pops=pops+pop_variations(food_consumed_pop,food)
-        if food<0 then
-            food=0
-        end
+        Pop1.Update()
         cycles=cycles+1
-    end
     
 end
 
@@ -90,15 +91,11 @@ function love.draw()
         love.graphics.print("Game is running",250,0) end
     love.graphics.setColor(1,0,0)
     love.graphics.setColor(1,1,1)
-    love.graphics.print(pops, font)
-    love.graphics.print(food,font,0,100) 
     love.graphics.print(cycles,font,0,150) 
-    pop_loss=pop_variations(food_consumed_pop,food)
-    love.graphics.print(Pop1.Food,font,600,0) 
-    love.graphics.print(Pop1.Number,font,600,50) 
-    love.graphics.print(Pop1.t_1,font,600,100) 
-    love.graphics.print(t_1, font,0,50)
-
-
+    for i= 1,3 do
+        love.graphics.print(Pops[i].Food,font,100*i,0) 
+        love.graphics.print(Pops[i].Number,font,100*i,50) 
+        love.graphics.print(Pops[i].t_1,font,100*i,100) 
+    end
 end
 
