2416d532216d9ba0f92c871fd371805d41141420
mak <davide.mattioli.2001@gmail.com>
2022-03-22 09:35:58 +0100

    Added a population control function and added more parameters

--- main.lua
+++ main.lua
@@ -1,60 +1,68 @@
 function love.load()
+    pause = false
     pops=5
-    food=100
+    food=50
     font=love.graphics.newFont(40)
     throughtput=1.0
-    food_consumed_pop=2
-    multi=5
+    food_consumed_pop=0.5
+    multi=1
     cycles=0
+    coefficent_innovation=0.01
+    coefficent_risparmio=0
  
 end
 
-local function throughtput(pop)
-    x=math.pow((1/1.01),pop)*multi;
-    
+local function throughtput(pop,cylces)
+    x=(math.pow((1/1.01),pop)+(math.cos(pop)+1)/50+ math.random(-0.01,0.015))*(multi);  
     return x
 end
-local function pop_variation(food_produced,food_consumed,food)
-    if (food-food_consumed)<0 then
-        food_deficit=math.abs(food-food_consumed)
-        pop_loss = math.floor(food_deficit/food_consumed_pop)
 
-    else 
-        pop_loss=0
-    end
-    return pop_loss
+local function pop_variations(food_consumed_pop,food)
+    pop_variation=math.floor((food/food_consumed_pop)/50)
+    return pop_variation
+
 end
+
+function innovation(money,coefficent_innovation)
+    a=1
+end
+
 
 
 function love.update(dt)
-
-    t_1=throughtput(pops)
-    food_produced=pops*t_1
-    food_consumed=food_consumed_pop*pops 
-    food=food+food_produced
-    food=food-food_consumed
-    if food>0 then
-        delta_pop=math.floor(pops/2)
-        pops=pops+1
-    else 
-        pop_loss=pop_variation(food_produced,food_consumed,food)
-        pops=pops-pop_loss
-        food=0
-        if pops<0 then
-            pops=0
+    --love.timer.sleep(0.05)
+    function love.keypressed(key, unicode)
+	 	if key == 'p' then 
+            pause = not pause 
         end
     end
-    cycles=cycles+1
-
+    if pause==false then
+        t_1=throughtput(pops,cycles)
+        food_produced=pops*t_1
+        food_consumed=food_consumed_pop*pops 
+        delta=food_produced-food_consumed
+        food=food+delta
+        pops=pops+pop_variations(food_consumed_pop,food)
+        if food<0 then
+            food=0
+        end
+        cycles=cycles+1
+    end
+    
 end
 
 function love.draw()
+    if pause then love.graphics.print("Game is paused",250,0) else 
+        love.graphics.print("Game is running",250,0) end
     love.graphics.setColor(1,0,0)
-    love.graphics.rectangle('fill',300,250,200,100)
     love.graphics.setColor(1,1,1)
     love.graphics.print(pops, font)
-    love.graphics.print(t_1, font,100,200)
-    love.graphics.print(food,font,200,100) 
-    love.graphics.print(cycles,font,250,150) 
+    love.graphics.print(food,font,0,100) 
+    love.graphics.print(cycles,font,0,150) 
+    pop_loss=pop_variations(food_consumed_pop,food)
+    love.graphics.print(pop_loss,font,0,200) 
+    love.graphics.print(t_1, font,0,50)
+
+
 end
 
