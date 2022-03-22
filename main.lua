9b46abd8085ebd8687d285da3bf721cb2009b8cb
mak <davide.mattioli.2001@gmail.com>
2022-03-22 10:56:13 +0100

    added the Pop object

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 function love.load()
     pause = false
-    pops=5
-    food=50
+    pops=10
+    food=500
     font=love.graphics.newFont(40)
     throughtput=1.0
     food_consumed_pop=0.5
@@ -9,13 +9,44 @@ function love.load()
     cycles=0
     coefficent_innovation=0.01
     coefficent_risparmio=0
+    Pop={Food=500, Number=10}
+    Pop1=Pop
+
+    function throughtput(pop,cylces)
+        x=(math.pow((1/1.01),pop)+(math.cos(pop)+1)/50+ math.random(-2,5))*(multi);  
+        return x
+    end
+
+    function Pop.Update()
+        function Pop.food()
+            Pop.t_1=throughtput(Pop.Number,cycles)
+            food_produced=Pop.Number*Pop.t_1
+            food_consumed=(food_consumed_pop+math.log(Pop.Number+1))*Pop.Number
+            delta=food_produced-food_consumed
+            Pop.Food=Pop.Food+delta
+            if Pop.Food<0 then
+                Pop.Food=0
+            end
+        end
+        function Pop.Change()
+            pops_variation=math.floor(((Pop.Food+delta)/food_consumed_pop)/50)
+            Pop.Number=Pop.Number+pops_variation
+            if Pop.Number<=0 then
+                Pop.Number=1
+            end
+        end
+        Pop.food()
+        Pop.Change()
+
+    end
+    
  
 end
 
-local function throughtput(pop,cylces)
-    x=(math.pow((1/1.01),pop)+(math.cos(pop)+1)/50+ math.random(-0.01,0.015))*(multi);  
-    return x
-end
+
+
+
+
 
 local function pop_variations(food_consumed_pop,food)
     pop_variation=math.floor((food/food_consumed_pop)/50)
@@ -29,8 +60,11 @@ end
 
 
 
+
+
 function love.update(dt)
     --love.timer.sleep(0.05)
+    Pop1.Update()
     function love.keypressed(key, unicode)
 	 	if key == 'p' then 
             pause = not pause 
@@ -39,7 +73,7 @@ function love.update(dt)
     if pause==false then
         t_1=throughtput(pops,cycles)
         food_produced=pops*t_1
-        food_consumed=food_consumed_pop*pops 
+        food_consumed=(food_consumed_pop+math.log(pops))*pops 
         delta=food_produced-food_consumed
         food=food+delta
         pops=pops+pop_variations(food_consumed_pop,food)
@@ -60,7 +94,9 @@ function love.draw()
     love.graphics.print(food,font,0,100) 
     love.graphics.print(cycles,font,0,150) 
     pop_loss=pop_variations(food_consumed_pop,food)
-    love.graphics.print(pop_loss,font,0,200) 
+    love.graphics.print(Pop1.Food,font,600,0) 
+    love.graphics.print(Pop1.Number,font,600,50) 
+    love.graphics.print(Pop1.t_1,font,600,100) 
     love.graphics.print(t_1, font,0,50)
 
 
