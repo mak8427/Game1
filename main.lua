e3e9753557a9f18b8987e9c42592e7544a0c0e84
mak8421 <davide.mattioli.2001@gmail.com>
2022-03-23 18:13:32 +0100

    added factory functions

--- main.lua
+++ main.lua
@@ -7,28 +7,56 @@ function love.load()
     pause = false
     font=love.graphics.newFont(40)
     throughtput=1.0
+    food_type_price=3
     food_consumed_pop=0.5
     multi=3
     cycles=0
     coefficent_innovation=0.01
     coefficent_risparmio=0
-    food_price=3
-    Pop={Food=500, Number=10, t_1=0}
+    Pop={food=500, number=10, t_1=0, money=0, salary=20}
+    ananas={output=0.9,value=3}
     Pop1=Pop
     Pops={}
     for i= 1,3 do
         Pops[i]=Pop
     end
 
-    farm_industry= {pops=pops, out=0 ,size=1, food_type=0.9}
+    farm_industry= {pops=Pops, out=0 ,size=1, food_type=ananas, capital=0, dividends_rate=0.5}
 
 
     function farm_industry.Update()
         function farm_industry.Output()
-            farm_industry.out=farm_industry.pops*food_type
+            farm_industry.out=farm_industry.pops[1].number*farm_industry.food_type.output 
         end
-        farm_industry.Output()
+        function farm_industry.Income()
+            farm_industry.income=farm_industry.out*farm_industry.food_type.value   
+        end
 
+        function farm_industry.Expences()
+            farm_industry.salary_expence=farm_industry.pops[1].number*farm_industry.pops[1].salary
+            farm_industry.pops[1].money=farm_industry.pops[1].money+farm_industry.salary_expence
+            farm_industry.expence=farm_industry.salary_expence
+
+        end
+
+        function farm_industry.Balance()
+            farm_industry.balance=farm_industry.income*farm_industry.expence
+        end
+
+        function farm_industry.Capital()
+            farm_industry.capital=farm_industry.capital+farm_industry.balance
+        end
+
+        function farm_industry.Investment()
+            farm_industry.dividends=farm_industry.capital*farm_industry.dividends_rate
+            farm_industry.pops[3]=farm_industry.pops[3].money+(farm_industry.dividends/farm_industry.pops[3].number)
+        end
+
+        farm_industry.Output()
+        farm_industry.Income()
+        farm_industry.Expences()
+        farm_industry.Balance()
+        farm_industry.Capital()
     end
 
 
@@ -38,24 +66,24 @@ function love.load()
     end
 
     function Pop.Update()
-        function Pop.food()
-            Pop.t_1=throughtput(Pop.Number,cycles)
-            food_produced=(Pop.Number+1)*Pop.t_1
-            food_consumed=(food_consumed_pop+math.log(Pop.Number+1))*Pop.Number
+        function Pop.Food()
+            Pop.t_1=throughtput(Pop.number,cycles)
+            food_produced=(Pop.number+1)*Pop.t_1
+            food_consumed=(food_consumed_pop+math.log(Pop.number+1))*Pop.number
             delta=food_produced-food_consumed
-            Pop.Food=Pop.Food+delta
-            if Pop.Food<0 then
-                Pop.Food=0
+            Pop.food=Pop.food+delta
+            if Pop.food<0 then
+                Pop.food=0
             end
         end
         function Pop.Change()
-            pops_variation=math.floor(((Pop.Food+delta)/food_consumed_pop)/5)
-            Pop.Number=Pop.Number+pops_variation
-            if Pop.Number<=0 then
-                Pop.Number=1
+            pops_variation=math.floor(((Pop.food+delta)/food_consumed_pop)/5)
+            Pop.number=Pop.number+pops_variation
+            if Pop.number<=0 then
+                Pop.number=1
             end
         end
-        Pop.food()
+        Pop.Food()
         Pop.Change()
 
     end
@@ -75,27 +103,26 @@ end
 
 
 function love.update(dt)
-    --love.timer.sleep(0.05)
-    function love.keypressed(key, unicode)
-	 	if key == 'p' then 
-            pause = not pause 
+        farm_industry.Update()
+        for i= 1,3 do
+            farm_industry.pops[i].Update()
         end
-    end
-        Pop1.Update()
         cycles=cycles+1
-    
 end
 
 function love.draw()
-    if pause then love.graphics.print("Game is paused",250,0) else 
-        love.graphics.print("Game is running",250,0) end
+
     love.graphics.setColor(1,0,0)
     love.graphics.setColor(1,1,1)
     love.graphics.print(cycles,font,0,150) 
     for i= 1,3 do
-        love.graphics.print(Pops[i].Food,font,100*i,0) 
-        love.graphics.print(Pops[i].Number,font,100*i,50) 
-        love.graphics.print(Pops[i].t_1,font,100*i,100) 
+        love.graphics.print( string.format(
+            "%.2f", Pops[i].food ),font,100*i,0) 
+        love.graphics.print(Pops[i].number,font,100*i,50)
     end
+    love.graphics.print(farm_industry.pops[1].money,font,400,50)
+    love.graphics.print(farm_industry.pops[2].money,font,400,100)
+    love.graphics.print(farm_industry.salary_expence,font,400,150)
+
 end
 
